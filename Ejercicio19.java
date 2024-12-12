a62a2cf05ba2953ce430fa0326b1670e42662864
Eliseu Martínez Monrabal <elimarmon@alu.edu.gva.es>
2024-12-12 09:10:55 +0100

    Acabado ejercicio 19

---
+++ Ejercicio19.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ejercicio19 {
+
+    /*
+    Realiza un programa que cuente los múltiplos de 3 desde el 1 hasta un número que introducimos
+    por teclado.
+    */
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Introduce un número: ");
+        int num = input.nextInt(); input.nextLine();
+        int multiplos = 0;
+        for (int i = 1; i <= num; i++) {
+          if (i % 3 == 0) multiplos++;
+        }
+        System.out.printf("Hay %d números múltiplos de 3.%n", multiplos);
+    }
+}
